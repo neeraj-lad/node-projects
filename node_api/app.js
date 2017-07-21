31af947cdbda77ea64bbdaddbf46f3db127d2fef
neeraj-lad <neeraj.lad@yandex.com>
2017-07-20 22:49:43 -0400

    Add post user call

--- node_api/app.js
+++ node_api/app.js
@@ -4,10 +4,22 @@ var server = restify.createServer();
 var users = {};
 var max_user_id = 0;
 
+
 server.get('/', function(req, res, next){
 	res.setHeader('content-type', 'application/json');
 	res.writeHead(200);
 	res.end(JSON.stringify(users));
+	return next();
+});
+
+server.post('/:user', function(req, res, next){
+	var user = req.params;
+	max_user_id++;
+	user.id = max_user_id;
+	users[user.id] = user;
+	res.setHeader('content-type', 'application/json');
+	res.writeHead(200);
+	res.end(JSON.stringify(user));
 	return next();
 });
 
